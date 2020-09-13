10940406ab0ce9bccdc6c744f085657744f21db6
Bryan Cervantes <bryanandkyle@gmail.com>
2020-09-13 20:15:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,7 @@ end
 
 def add_element_to_start_of_array(array, element)
    create_an_array = ["bob", "mo", "lo", "sh"]
-   
+   create_an_array 
 end
 
 def remove_element_from_end_of_array(array)

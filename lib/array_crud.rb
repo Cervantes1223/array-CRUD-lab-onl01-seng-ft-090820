ef139c7f4ca29f4a8cb5c3d1d01dcd0804fbc73a
Bryan Cervantes <bryanandkyle@gmail.com>
2020-09-13 20:21:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,7 +24,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+   create_an_array = ["wow", "bob", "mo", "lo", "sh"]
+   create_an_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
